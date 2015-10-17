3911798940b6270397b3b1e6767d49afbc5637a0
akasfei <akasfei@gmail.com>
2015-10-17 08:43:40 -0700

    Fixed sols digits

--- dist/js/application.js
+++ dist/js/application.js
@@ -2,7 +2,7 @@
   var base = new Date('2015-09-17T08:00:00.000-0700');
   var updateSol = function () {
     $('#current-sol').text(parseInt((Date.now() - base.getTime()) / (1000 * 60 * 60 * 24)));
-    $('#current-sol-dec').text('.' + parseInt((Date.now() - base.getTime()) % (1000 * 60 * 60 * 24) / (6 * 60 * 24)));
+    $('#current-sol-dec').text('.' + ('0000'+parseInt((Date.now() - base.getTime()) % (1000 * 60 * 60 * 24) / (6 * 60 * 24))).slice(-4));
   };
   $(document).ready(function () {
     updateSol();
